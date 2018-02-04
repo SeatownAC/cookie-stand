e8f3c555ab1f91e0c38a109189316fcbb6a50f17
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-02-04 14:18:20 -0800

    changed indents

--- newapp.js
+++ newapp.js
@@ -108,21 +108,20 @@ function makeTotalRow() {
    var finalTotal = 0;
 
    for (var i = 0; i < hours.length; i++) {
-   totalCookiesPerHour = 0;
-   for (var j = 0; j < allLocations.length; j++) {
-       totalCookiesPerHour += allLocations[j].cookiesPerHour[i];
+        totalCookiesPerHour = 0;
+        for (var j = 0; j < allLocations.length; j++) {
+            totalCookiesPerHour += allLocations[j].cookiesPerHour[i];
+        }
+        tdEl = document.createElement('td');
+        tdEl.textContent = totalCookiesPerHour;
+        trEl.appendChild(tdEl);
+        finalTotal += totalCookiesPerHour;
 
-   }
-   tdEl = document.createElement('td');
-   tdEl.textContent = totalCookiesPerHour;
-   trEl.appendChild(tdEl);
-   finalTotal += totalCookiesPerHour;
-
-   }
-   tdEl = document.createElement('td');
-   tdEl.textContent = finalTotal;
-   trEl.appendChild(tdEl);
-   cookieshops.appendChild(trEl);
+    }
+    tdEl = document.createElement('td');
+    tdEl.textContent = finalTotal;
+    trEl.appendChild(tdEl);
+    cookieshops.appendChild(trEl);
 
 
 }
@@ -150,18 +149,18 @@ function handleSubmitForm(event) {
     var avgCookiesPerHour = parseFloat(event.target.avgcookiesperhour.value);
 
   
-new Location(name, minCustHour, maxCustHour, avgCookiesPerHour); 
+    new Location(name, minCustHour, maxCustHour, avgCookiesPerHour); 
 
-var endRow = document.getElementById('endRow');
-cookieshops.removeChild(endRow);
+    var endRow = document.getElementById('endRow');
+    cookieshops.removeChild(endRow);
 
-makeDataRow(allLocations.length - 1);
-makeTotalRow();
+    makeDataRow(allLocations.length - 1);
+    makeTotalRow();
 
-event.target.name.value = null;
-event.target.minCustHour.value = null;
-event.target.maxCustHour.value = null;
-event.target.avgCookiesPerHour.value = null;
+    event.target.name.value = null;
+    event.target.minCustHour.value = null;
+    event.target.maxCustHour.value = null;
+    event.target.avgCookiesPerHour.value = null;
 
 // for(var i = cookieshops.rows.length; i=0; i--) {
 //     cookieshops.deleteRow(i-1)
